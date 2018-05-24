4b361f606c54d632aa7ca41241f84c77252091a9
mufichmj <mufichmj@rose-hulman.edu>
2018-05-23 22:27:19 -0400

    mufich-m4

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -421,7 +421,7 @@ def practice_problem4d(sequence):
     sum = 0
     for k in range(len(sequence)):
         if is_prime(sequence[k]) and is_prime(sequence[k+1]) \
-                and is_prime(sequence[k]) != is_prime(sequence[k + 1]):
+                and sequence[k] != sequence[k + 1]:
             sum = sum + sequence[k]
 
     return sum
